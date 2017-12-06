8162be50b2926063bdf15547052997479b4a8573
ShoheiMaeda <Shohei-Maeda-ss@excite.co.jp>
2017-12-06 21:48:04 +0900

    Initial commit

---
+++ html/src/jp/maeda/shohei/jumpactiongame/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package jp.maeda.shohei.jumpactiongame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import jp.maeda.shohei.jumpactiongame.JumpActionGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new JumpActionGame();
+        }
+}
